0d156c0b189eb856d0309da640289c6214cbaad0
shashank-mishra123 <kanhapmishra2022@gmail.com>
2026-01-17 11:57:24 +0530

    name mangling

---
+++ filehandling/13oops.py
@@ -0,0 +1,12 @@
+# show how name magling working in pyhton
+class employee():
+    def __init__(self):
+        self.__varible=500
+    
+"""def get_varible(self):
+        print(self.__varible)
+        """
+
+emp = employee()
+
+print(emp._employee__varible)
